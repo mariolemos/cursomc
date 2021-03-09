da773fe7f0fcfee1934e312b31b75a34ad10a7b1
mariolemos <lemosmm64@gmail.com>
2021-03-09 16:15:41 -0300

    Validação sintatica com Bean Validetion

--- src/main/java/com/mariolemos/cursomc/services/CategoriaService.java
+++ src/main/java/com/mariolemos/cursomc/services/CategoriaService.java
@@ -12,6 +12,7 @@ import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.mariolemos.cursomc.domain.Categoria;
+import com.mariolemos.cursomc.dto.CategoriaDTO;
 import com.mariolemos.cursomc.repositories.CategoriaRepository;
 import com.mariolemos.cursomc.services.exceptions.DataIntegrityException;
 import com.mariolemos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);	
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 	
